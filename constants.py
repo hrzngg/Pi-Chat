ed2252faa94619ffc479da7b6be4797fa7dadd67
hrzn <xabhay9@gmail.com>
2025-10-25 17:06:48 +0200

    Create constants.py
    Store all the constants used here!

---
+++ constants.py
@@ -0,0 +1,7 @@
+OPEN_API_KEY = "";
+PICOVOICE_API_KEY = "fjRModTxjurjZY/MIknI5bCL3J33HQExiW0VhfKdj4CDUDFds3EqEg==";
+WAKEWORD = "alexa"
+WAKEWORDSOUND = "/home/xpi5/workspace/Sounds/innit2.wav" 
+WAKEWORDSOUND2 = "/home/xpi5/workspace/Sounds/innit.wav"
+MIC_INDEX = 0
+SPEECH_DURATION = 5
